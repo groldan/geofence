633d27fadbb81f010ff0a117e8614214b3f18eab
Gabriel Roldan <gabriel.roldan@gmail.com>
2021-04-09 17:49:18 -0300

    Move spring-cloud dependencies to a cloud-native maven profile

--- src/webapp/src/main/java/org/geoserver/geofence/app/GeofenceServiceApplication.java
+++ src/webapp/src/main/java/org/geoserver/geofence/app/GeofenceServiceApplication.java
@@ -1,13 +1,9 @@
 package org.geoserver.geofence.app;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.bus.BusAutoConfiguration;
-import org.springframework.cloud.bus.BusRefreshAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = {BusAutoConfiguration.class, BusRefreshAutoConfiguration.class})
 public class GeofenceServiceApplication {
 
     public static void main(String[] args) {
