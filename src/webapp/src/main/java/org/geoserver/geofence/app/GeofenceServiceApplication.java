82c5f235322248601a859967eb00084de6d7d7f3
Gabriel Roldan <gabriel.roldan@gmail.com>
2021-04-09 16:35:35 -0300

    Initial import of spring-boot web app as created with https://start.spring.io/

---
+++ src/webapp/src/main/java/org/geoserver/geofence/app/GeofenceServiceApplication.java
@@ -0,0 +1,17 @@
+package org.geoserver.geofence.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.bus.BusAutoConfiguration;
+import org.springframework.cloud.bus.BusRefreshAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {BusAutoConfiguration.class, BusRefreshAutoConfiguration.class})
+public class GeofenceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeofenceServiceApplication.class, args);
+	}
+
+}
